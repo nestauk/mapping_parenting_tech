6d06f5327c657b1d7ad08534734c3b47138a8ba4
Mat Hickman <mat.hickman@gmail.com>
2021-12-10 12:46:21 +0000

    Setup Nesta Data Science cookiecutter

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/mapping_parenting_tech
+   License <license>
+   Github <https://github.com/nestauk/mapping_parenting_tech/>
+
+mapping_parenting_tech
+===============================
+
+Analysis of apps and other digital tools parents can use to improve their children's home learning environment
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,26 @@
+"""mapping_parenting_tech."""
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+BASE_DIR = Path(__file__).parent
+
+
+setup(
+    name="mapping_parenting_tech",
+    long_description=open(BASE_DIR / "README.md").read(),
+    install_requires=read_lines(BASE_DIR / "requirements.txt"),
+    extras_require={"dev": read_lines(BASE_DIR / "requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="Analysis of apps and other digital tools parents can use to improve their children's home learning environment",
+    author="Nesta",
+    license="MIT",
+)
