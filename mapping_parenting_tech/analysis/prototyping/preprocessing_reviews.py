d02567c029a4cb678a43c30dbbab1bf9ee02618b
beingkk <beingkk@users.noreply.github.com>
2021-12-16 15:23:59 +0000

    updated the notebook

--- mapping_parenting_tech/analysis/prototyping/preprocessing_reviews.py
+++ mapping_parenting_tech/analysis/prototyping/preprocessing_reviews.py
@@ -37,28 +37,16 @@ from spacy.tokens.span import Span
 from toolz import pipe
 from gensim import models
 import itertools
+import pickle
+from gensim.models.phrases import FrozenPhrases
 
 nlp = spacy.load("en_core_web_sm")
 
-INPUTS_PATH = PROJECT_DIR / "inputs/data/kaggle/reviews.csv"
 PARENT_INPUTS_PATH = PROJECT_DIR / "outputs/data"
+CLUTERING_OUTPUTS_PATH = PROJECT_DIR / "outputs/data/clustering"
 # -
 
 # # Load and examine data
-
-# ### Test data
-
-reviews_df = pd.read_csv(INPUTS_PATH)
-
-reviews_df.info()
-
-profile = ProfileReport(reviews_df, title=f"Profiling report of {INPUTS_PATH.name}")
-with open(
-    INPUTS_PATH.parent / f"{INPUTS_PATH.stem}_profiling_report.html", "w"
-) as outfile:
-    outfile.write(profile.to_html())
-
-# ### Fetched review data
 
 # +
 with open(PARENT_INPUTS_PATH / "play_store_ids.json", "r") as infile:
@@ -66,21 +54,24 @@ with open(PARENT_INPUTS_PATH / "play_store_ids.json", "r") as infile:
 
 with open(PARENT_INPUTS_PATH / "play_store_reviews.json", "r") as infile:
     play_store_reviews = json.load(infile)
+
+with open(PARENT_INPUTS_PATH / "play_store_details.json", "r") as infile:
+    play_store_details = json.load(infile)
 # -
 
 len(play_store_ids["Parenting apps"])
 
 # Convert dictionary to dataframe
-df = pd.DataFrame()
+reviews_df = pd.DataFrame()
 for app_id, app_data in play_store_reviews.items():
     df_app = pd.DataFrame(app_data)
     df_app["appId"] = app_id
-    df = df.append(df_app, ignore_index=True)
+    reviews_df = reviews_df.append(df_app, ignore_index=True)
 
 
-len(df)
+len(reviews_df)
 
-profile = ProfileReport(df, title=f"Profiling report of Play Store apps")
+profile = ProfileReport(reviews_df, title=f"Profiling report of Play Store apps")
 with open(
     PARENT_INPUTS_PATH / f"play_store_reviews_profiling_report.html", "w"
 ) as outfile:
@@ -98,10 +89,6 @@ with open(
 #
 # - Topic modelling
 #
-
-reviews_list = df.content.to_list()
-
-reviews_list[999]
 
 # +
 ### Preprocessing
@@ -139,7 +126,7 @@ def clean_sentence(text: Span) -> str:
     """Pipeline to process a single sentence"""
     return pipe(
         text,
-        # remove_stopwords,
+        remove_stopwords,
         lemmatise_and_join,
         textacy_preproc_pipeline,
     )
@@ -148,9 +135,7 @@ def clean_sentence(text: Span) -> str:
 def review_preprocessor(doc: Doc) -> (Span, Iterator[str]):
     """Text preprocessing pipeline for a review"""
     sents = split_sentences(doc)
-    return sents, remove_empty_sentences(
-        [clean_sentence(sent).lower() for sent in sents]
-    )
+    return sents, [clean_sentence(sent).lower() for sent in sents]
 
 
 ### Tokenising
@@ -181,156 +166,73 @@ def make_ngram(
     return list(tokenised), ngram_phraser
 
 
-# def ngrammer(text, ngram_phraser, nlp=None):
-#     return ngram_phraser[process_text(nlp(remove_newline(text)))
+def ngrammer(text: str, ngram_phraser: FrozenPhrases, nlp: spacy.lang):
+    """Coverts free text to preprocessed ngrammed sentences"""
+    # Preprocess sentences
+    _, preprocessed_sents = review_preprocessor(nlp(text))
+    # Split sentences into words
+    sents_tokenised_words = [sent.split() for sent in preprocessed_sents]
+    # Join up words into ngrams/phrases
+    return [ngram_phraser[sent] for sent in sents_tokenised_words]
+
+
 # -
 
-txt = df[-df.content.isnull()].content.to_list()
+# ### Export reviews, split by sentence
 
-selected_reviews_df = df[-df.content.isnull()]
-
-# +
-### Export reviews, split by sentence
-# -
-
-df.content.isnull().sum()
-
-reviews_sentences = [split_sentences(doc) for doc in nlp.pipe(txt)]
-
-reviews_sentences_str = [[t.text for t in review] for review in reviews_sentences]
-
-# +
-# selected_reviews_df.iloc[21]
-# -
-
-clustering_inputs = PROJECT_DIR / "outputs/data/clustering"
+# Select only reviews that have text and remove duplicates
+selected_reviews_df = reviews_df[-reviews_df.content.isnull()].drop_duplicates(
+    "reviewId"
+)
 
 selected_reviews_df.reviewId.duplicated().sum()
 
-import pickle
+# Select review texts
+review_texts = selected_reviews_df.content.to_list()
 
-selected_reviews_df.to_csv(
-    clustering_inputs / "reviews_for_clustering.csv", index=False
-)
-pickle.dump(
-    reviews_sentences_str,
-    open(clustering_inputs / "reviews_for_clustering_sentences.pickle", "wb"),
-)
+# Process reviews
+processed_reviews = [review_preprocessor(doc) for doc in nlp.pipe(review_texts)]
 
-# +
-### Tokenise reviews
-# -
+# Convert split sentences from Span to strings
+reviews_sentences = [r[0] for r in processed_reviews]
+reviews_sentences_str = [[t.text for t in review] for review in reviews_sentences]
 
-preprocessed_reviews = [review_preprocessor(doc) for doc in nlp.pipe(txt)]
-tokenised_reviews = [[sent.split() for sent in sents] for sents in preprocessed_reviews]
 
+## Tokenise the sentences
+# Preprocessed sentences
+reviews_sentences_preprocessed = [r[1] for r in processed_reviews]
+# Tokenise at the level of words
+tokenised_reviews = [
+    [sent.split() for sent in sents] for sents in reviews_sentences_preprocessed
+]
 tokenised_reviews_flat = [sent for sents in tokenised_reviews for sent in sents]
-
+# Find ngrams/phrases
 tok_ngram, ngram_phraser = make_ngram(tokenised_reviews_flat, n_gram=3)
-
-# +
-# def ngrammer(text, ngram_phraser, nlp=None):
-#     if nlp is None:
-#         nlp = setup_spacy_model(DEF_LANGUAGE_MODEL)
-#     return ngram_phraser[process_text(nlp(remove_newline(text)))]
-# -
-
-ngram_phraser[tokenised_reviews[0][2]]
-
-# +
-## Topic modelling
-# -
-
-
-# +
-# for review in tokenised_reviews:
-#     ' '.join([ngram_phraser[sent] for sent in review]
-# -
-
+# Tokenise sentences at the level of ngrams/phrases
 ngram_reviews = [
     [ngram_phraser[sent] for sent in review] for review in tokenised_reviews
 ]
 
-review_corpus = [list(itertools.chain(*ngram_review)) for ngram_review in ngram_reviews]
-
-txt[50]
-
-len(review_corpus)
-
-# +
-# review_corpus[50]
-# -
-
-review_corpus[0]
-
-
-# ## Simple topic model
-
-# +
-def train_model(mdl, iterations=1000, step=20):
-    """Let's train the model"""
-    for i in range(0, iterations, step):
-        logging.info("Iteration: {:04}, LL per word: {:.4}".format(i, mdl.ll_per_word))
-        mdl.train(step)
-    logging.info(
-        "Iteration: {:04}, LL per word: {:.4}".format(iterations, mdl.ll_per_word)
-    )
-    mdl.summary()
-    return mdl
-
-
-def get_topic_term_dists(mdl):
-    return np.stack([mdl.get_topic_word_dist(k) for k in range(mdl.k)])
-
-
-def get_doc_topic_dists(mdl):
-    doc_topic_dists = np.stack([doc.get_topic_dist() for doc in mdl.docs])
-    doc_topic_dists /= doc_topic_dists.sum(axis=1, keepdims=True)
-    return doc_topic_dists
-
-
-def make_pyLDAvis(mdl, fpath=PROJECT_DIR / "outputs/data/ldavis_tomotopy.html"):
-    topic_term_dists = get_topic_term_dists(mdl)
-    doc_topic_dists = get_doc_topic_dists(mdl)
-    doc_lengths = np.array([len(doc.words) for doc in mdl.docs])
-    vocab = list(mdl.used_vocabs)
-    term_frequency = mdl.used_vocab_freq
-
-    prepared_data = pyLDAvis.prepare(
-        topic_term_dists,
-        doc_topic_dists,
-        doc_lengths,
-        vocab,
-        term_frequency,
-        start_index=0,  # tomotopy starts topic ids with 0, pyLDAvis with 1
-        sort_topics=False,  # IMPORTANT: otherwise the topic_ids between pyLDAvis and tomotopy are not matching!
-    )
-    pyLDAvis.save_html(prepared_data, str(fpath))
-
-
-def print_model_info(mdl):
-    print(
-        "Num docs:{}, Num Vocabs:{}, Total Words:{}".format(
-            len(mdl.docs), len(mdl.used_vocabs), mdl.num_words
-        )
-    )
-    print("Removed Top words: ", *mdl.removed_top_words)
-
-
-# -
-
-# Create a corpus instance
-corpus = tomotopy.utils.Corpus()
-for doc in review_corpus:
-    corpus.add_doc(doc)
-
-topic_model = tomotopy.LDAModel(min_df=5, rm_top=40, k=40, corpus=corpus, seed=1111)
-topic_model.train(0)
-print_model_info(topic_model)
-
-train_model(topic_model, iterations=1000, step=100)
-
-make_pyLDAvis(
-    topic_model,
-    fpath=PROJECT_DIR / "outputs/models/topic_modelling/topic_modelling.html",
+reviews_processed = selected_reviews_df.copy()
+reviews_processed["title"] = reviews_processed["appId"].apply(
+    lambda x: play_store_details[x]["title"]
 )
+reviews_processed["content_sentence"] = reviews_sentences_str
+reviews_processed["content_sentence_tokens"] = ngram_reviews
+reviews_processed = reviews_processed.drop(["userName", "userImage"], axis=1).explode(
+    ["content_sentence", "content_sentence_tokens"]
+)
+
+
+reviews_processed.head(2)
+
+
+len(reviews_processed)
+
+reviews_processed.to_csv(
+    CLUTERING_OUTPUTS_PATH / "reviews_for_clustering_processed.csv", index=False
+)
+
+# +
+# selected_reviews_df.to_csv(CLUTERING_OUTPUTS_PATH / 'reviews_for_clustering.csv', index=False)
+# pickle.dump(reviews_sentences_str, open(CLUTERING_OUTPUTS_PATH / 'reviews_for_clustering_sentences.pickle', 'wb'))
