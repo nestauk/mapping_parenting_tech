31d298a01c5403f1e68dcc04ebba67ddb02e047a
Mat Hickman <mat.hickman@gmail.com>
2021-12-16 13:22:30 +0000

    Addresses feedback from Karlis on PR

--- mapping_parenting_tech/analysis/prototyping/play_store/get_play_store_apps.py
+++ mapping_parenting_tech/analysis/prototyping/play_store/get_play_store_apps.py
@@ -21,22 +21,21 @@
 # Do imports and set file locations
 
 # %%
-from pathlib import Path
 from google_play_scraper import Sort, app, reviews_all
 from tqdm import tqdm
 import re
 import json
-import sys
 from mapping_parenting_tech import PROJECT_DIR, logging
+
 INPUT_PATH = PROJECT_DIR / "inputs/data"
-OUTPUT_PATH =PROJECT_DIR / "outputs/data"
+OUTPUT_PATH = PROJECT_DIR / "outputs/data"
 STORE_PATH = "play_store/"
 PLAY_DATA_FILE = "playstore_innerHTML.html"
 OUTPUT_PLAY_IDS_FILE = "play_store_ids.json"
 OUTPUT_PLAY_DETAILS_FILE = "play_store_details.json"
 OUTPUT_PLAY_REVIEWS_FILE = "play_store_reviews.json"
 
-input_data = INPUT_PATH.expanduser() / STORE_PATH / PLAY_DATA_FILE
+input_data = INPUT_PATH / STORE_PATH / PLAY_DATA_FILE
 
 if input_data.exists():
     logging.info("Ok to proceed")
@@ -52,20 +51,21 @@ else:
 with open(input_data, "r") as infile:
     html = infile.read()
 
-link_targets = re.findall(r"(?<=href=\"\/store\/apps\/details\?id=)(.*?)(?=\")", html)
+re_pattern = r"(?<=href=\"\/store\/apps\/details\?id=)(.*?)(?=\")"
+link_targets = re.findall(re_pattern, html)
 app_ids = list(dict.fromkeys(link_targets))
 output_ids = {"Parenting apps": app_ids}
-print("Apps ids parsed ok")
+logging.info("Apps ids parsed ok")
 
 # %% [markdown]
 # Save app ids to JSON file
 
 # %%
-output_target = OUTPUT_PATH.expanduser() / OUTPUT_PLAY_IDS_FILE
+output_target = OUTPUT_PATH / OUTPUT_PLAY_IDS_FILE
 with open(output_target, "w") as output_file:
     json.dump(output_ids, output_file)
 output_file.close
-print("App ids saved to", output_target)
+print("App ids saved to", output_target)  # logging.info throws an error here
 
 # %% [markdown]
 # Retrieve app details
@@ -87,23 +87,25 @@ for app_id in tqdm(output_ids["Parenting apps"], desc="Retrieving app details"):
             most_reviews["id"] = app_id
             most_reviews["count"] = app_details["reviews"]
 
-    except Exception as e:
-        print(f"Error on app id {app_id}: {e} {repr(e)}")
+    except Exception as e:  # needs modifying to capture specific errors
+        logging.info(f"Error on app id {app_id}: {e} {repr(e)}")
         remove_apps.append(app_id)
 
 for app_id in remove_apps:
     output_ids["Parenting apps"].remove(app_id)
 
-print(
+logging.info(
     f"There are {review_counter} reviews available for {len(output_ids['Parenting apps'])} apps. {len(remove_apps)} apps were removed"
 )
-print(f"App id {most_reviews['id']} had the most reviews with {most_reviews['count']}")
+logging.info(
+    f"App id {most_reviews['id']} had the most reviews with {most_reviews['count']}"
+)
 
 # %% [markdown]
 # Save app details
 
 # %%
-output_target = OUTPUT_PATH.expanduser() / OUTPUT_PLAY_DETAILS_FILE
+output_target = OUTPUT_PATH / OUTPUT_PLAY_DETAILS_FILE
 with open(output_target, "w") as output_file:
     json.dump(all_app_details, output_file, indent=2, default=str)
 output_file.close
@@ -124,20 +126,20 @@ for app_id in tqdm(output_ids["Parenting apps"], desc="Retrieving app reviews"):
             sleep_milliseconds=0,  # defaults to 0
             lang="en",
             country="gb",
-            sort=Sort.MOST_RELEVANT  # defaults to Sort.MOST_RELEVANT
+            sort=Sort.NEWEST  # defaults to Sort.MOST_RELEVANT
             # filter_score_with=5 # defaults to None(means all score)
         )
         all_app_reviews.update({app_id: app_reviews})
         running_total += len(app_reviews)
 
     except Exception as e:
-        print(f"Error on app id {app_id}: {e} {repr(e)}")
+        logging.info(f"Error on app id {app_id}: {e} {repr(e)}")
 
-print(f"Retrieved {running_total} reviews")
+logging.info(f"Retrieved {running_total} reviews")
 
 
 # %%
-output_target = OUTPUT_PATH.expanduser() / OUTPUT_PLAY_REVIEWS_FILE
+output_target = OUTPUT_PATH / OUTPUT_PLAY_REVIEWS_FILE
 with open(output_target, "w") as output_file:
     json.dump(all_app_reviews, output_file, indent=2, default=str)
 output_file.close
