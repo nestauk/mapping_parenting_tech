d145de89ef4a6246f672d0ffa771bf213743863b
Karlis Kanders <beingkk@users.noreply.github.com>
2022-04-14 17:42:35 +0100

    updating jupytext (#42)
    * updating jupytext
    
    * updating jupytext

--- mapping_parenting_tech/analysis/prototyping/app_feedback_analysis.py
+++ mapping_parenting_tech/analysis/prototyping/app_feedback_analysis.py
@@ -3,11 +3,12 @@
 #   jupytext:
 #     cell_metadata_filter: -all
 #     comment_magics: true
+#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.13.7
+#       jupytext_version: 1.13.8
 #   kernelspec:
 #     display_name: 'Python 3.8.12 64-bit (''mapping_parenting_tech'': conda)'
 #     language: python
