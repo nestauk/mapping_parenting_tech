bb49ba11a2699ee35a853735500aa7cacc231f39
beingkk <beingkk@users.noreply.github.com>
2022-06-13 17:27:17 +0100

    reorganised

--- mapping_parenting_tech/analysis/prototyping/app_feedback_analysis.py
+++ mapping_parenting_tech/analysis/prototyping/app_feedback_analysis.py
@@ -673,6 +673,46 @@ fig = pu.configure_plots(fig, chart_title, chart_subtitle)
 fig
 
 # %%
+top_app_dict = {}
+for cat in t_dfs_.Category.unique():
+    top_app_dict[cat] = (
+        t_dfs_.query("Category == @cat")
+        .sort_values("Installations", ascending=False)
+        .iloc[0]
+        .title
+    )
+
+# %%
+alt_text = " ".join(
+    [
+        "This graph shows a bar chart with the number of different types of apps for toddlers and parents in the UK Google Play Store.",
+        "The categories of apps aimed at children and the number of apps in each category are the following:",
+        "; ".join(
+            [
+                f"{row['Category']}: {row['Number of apps']}"
+                for i, row in app_counts_all.query(
+                    "`App user` == 'Children'"
+                ).iterrows()
+            ]
+        )
+        + ".",
+        "The categories of apps aimed at parents and the number of apps in each category are the following:",
+        "; ".join(
+            [
+                f"{row['Category']}: {row['Number of apps']}"
+                for i, row in app_counts_all.query("`App user` == 'Parents'").iterrows()
+            ]
+        )
+        + ".",
+    ]
+)
+alt_text
+
+# %%
+alt_text = "; ".join([f"{cat}: {top_app_dict[cat]}" for cat in top_app_dict])
+alt_text
+
+# %%
 table_name = "top_apps_per_category_children"
 AltairSaver.save(fig, table_name, filetypes=["html", "svg", "png"])
 
@@ -690,6 +730,44 @@ df = t_dfs_[
     ]
 ]
 utils.save_data_table(df, table_name)
+
+# %%
+chart_title = "Five most popular parenting apps in each category"
+chart_subtitle = "Apps with the largest number of installations"
+url = "https://raw.githubusercontent.com/beingkk/test/main/top_apps_per_category_children.csv"
+fig = (
+    alt.Chart(
+        url,
+        width=450,
+        height=500,
+    )
+    .mark_image(
+        width=40,
+        height=40,
+    )
+    .encode(
+        x=alt.X(
+            "ordering:Q",
+            scale=alt.Scale(domain=(-1, 9)),
+            axis=None,
+        ),
+        y=alt.Y(
+            f"{labels_title}:N",
+            sort=app_counts.Category.to_list(),
+            axis=alt.Axis(labelLimit=200),
+        ),
+        url="icon:N",
+        href="url:N",
+        tooltip=["title:N", "Description:N", "Installations:Q", "Score:N"],
+    )
+)
+
+fig = pu.configure_plots(fig, chart_title, chart_subtitle)
+fig
+
+# %%
+table_name = "top_apps_per_category_children_url"
+AltairSaver.save(fig, table_name, filetypes=["html", "svg", "png"])
 
 # %%
 n_top = 5
@@ -825,6 +903,15 @@ dfe_apps_df = (
     )
 )
 dfe_apps_df
+
+# %%
+alt_text = "; ".join(
+    [
+        f"{row.title} ({row.installs} installations, score {row.score:.2f})"
+        for i, row in dfe_apps_df.iterrows()
+    ]
+)
+alt_text
 
 # %%
 dfe_fig = (
@@ -1118,6 +1205,26 @@ fig
 table_name = "ads_iap_free_children_apps"
 utils.save_data_table(apps_access_plot.drop("variable", axis=1), table_name)
 AltairSaver.save(fig, table_name, filetypes=["html", "svg", "png"])
+
+# %%
+apps_access_plot.head(2)
+
+# %%
+p = []
+for cat in apps_access_plot.Category.unique():
+    p.append(
+        cat
+        + ": "
+        + "; ".join(
+            [
+                f'{row.Percentage*100:.0f}% {row["Free, ads or purchases"]}'
+                for i, row in apps_access_plot.query("Category == @cat")
+                .sort_values("Free, ads or purchases")
+                .iterrows()
+            ]
+        )
+    )
+". ".join(p)
 
 # %%
 apps_access.sort_values(["User", "Free"], ascending=False)
@@ -1581,6 +1688,21 @@ sort_order = (
 )[labels_title].to_list()
 
 # %%
+reviews_per_year_by_cluster.query("Category == @cat and Year == 2019")[
+    "Number of reviews (thousands)"
+].iloc[0]
+
+# %%
+al_text = "; ".join(
+    [
+        f'{cat}: {reviews_per_year_by_cluster.query("Category == @cat and Year == 2019")["Number of reviews (thousands)"].iloc[0]*1000:.0f} and {reviews_per_year_by_cluster.query("Category == @cat and Year == 2020")["Number of reviews (thousands)"].iloc[0]*1000:.0f}'
+        for cat in reviews_per_year_by_cluster.Category.unique()
+    ]
+)
+al_text
+
+
+# %%
 # Short term trend (2019 -> 2020)
 growth_title = "Growth"
 
@@ -1645,12 +1767,17 @@ fig = (
             "fontSize": 15,
         },
     )
-    # .configure_axis(
-    #     gridDash=[1, 7],
-    #     gridColor="grey",
-    # )
-    # .configure_view(stroke=None, strokeWidth=0)
-    # .interactive()
+    .configure_axis(
+        gridDash=[1, 7],
+        gridColor="grey",
+        labelFontSize=pu.FONTSIZE_NORMAL,
+        titleFontSize=pu.FONTSIZE_NORMAL,
+    )
+    .configure_legend(
+        titleFontSize=pu.FONTSIZE_NORMAL,
+        labelFontSize=pu.FONTSIZE_NORMAL,
+    )
+    .configure_view(strokeWidth=0)
 )
 
 fig
@@ -1671,7 +1798,7 @@ chart_subtitle = ""
 fig_growth = (
     alt.Chart(
         review_growth_yoy,
-        width=150,
+        width=200,
         height=250,
     )
     .mark_bar(color=color)
@@ -1703,12 +1830,17 @@ fig_growth = (
             "fontSize": 15,
         },
     )
-    # .configure_axis(
-    #     gridDash=[1, 7],
-    #     gridColor="grey",
-    # )
-    # .configure_view(strokeWidth=0)
-    # .interactive()
+    .configure_axis(
+        gridDash=[1, 7],
+        gridColor="grey",
+        labelFontSize=pu.FONTSIZE_NORMAL,
+        titleFontSize=pu.FONTSIZE_NORMAL,
+    )
+    .configure_legend(
+        titleFontSize=pu.FONTSIZE_NORMAL,
+        labelFontSize=pu.FONTSIZE_NORMAL,
+    )
+    .configure_view(strokeWidth=0)
 )
 
 fig_growth
@@ -1718,6 +1850,14 @@ importlib.reload(utils)
 table_name = "children_app_growth_2019_2020"
 utils.save_data_table(review_growth_yoy, f"{table_name}")
 AltairSaver.save(fig_growth, table_name, filetypes=["html", "svg", "png"])
+
+# %%
+"; ".join(
+    [
+        f'{cat}: {review_growth_yoy.query("Category == @cat").Growth.iloc[0]*100:.0f}%'
+        for cat in review_growth_yoy.Category.unique()
+    ]
+)
 
 # %%
 fig_final = (
--- mapping_parenting_tech/analysis/prototyping/readme.md
+++ mapping_parenting_tech/analysis/prototyping/readme.md
@@ -1,12 +1,1 @@
 # Analysing the Apple App and Google Play stores
-
-## Information to save
-
-- { id: {
-  - platform
-    - iOS or Android
-  - description
-  - ratings
-    - [#1, #2, #3, #4, #5]
-  - [reviews]
-- }
